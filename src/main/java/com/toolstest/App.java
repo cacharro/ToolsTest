f81a9afe17e6fb492a4e2f51faebb758ffb12d0d
cacharro <cesarhf@gmail.com>
2019-04-04 10:39:12 +0200

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -8,6 +8,7 @@ public class App {
     private String name;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        name = "Cesar";
+        System.out.println(name);
     }
 }
